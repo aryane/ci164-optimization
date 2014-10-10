1eb7ab41e8cd5c882fb4294b4856504d292b72e6
Kevin Katzer <kk11@inf.ufpr.br>
2014-10-10 17:31:38 -0300

    gradSolver.c: Alterações para adição de biblioteca.
    Sign-off-by: Kevin Katzer <kk11@inf.ufpr.br>

--- gradSolver.c
+++ gradSolver.c
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 #include <string.h>
 #include "mat_utils.h"
+#include "grad_f.h"
 
 /**
  * @file gradSolver.c
@@ -11,32 +12,6 @@
  * @author Aryane Ast dos Santos
  * @author Kevin Katzer
  */
-
-/**
- * @brief Calcula norma do resíduo de um sistema na forma Ax = b
- * @param *A Matriz.
- * @param *b vetor b.
- * @param *x Vetor solução.
- * @param n Dimensão da matriz.
- */
-double calcResidualNorm(double *A, double *b, double *x, int n) {
-    double r[n], norm, sum=0.0;
-    int i, j;
-
-    for (i=0; i<n; ++i) {
-        r[i] = b[i];
-        for (j=0; i<n; ++i) {
-            r[i] -= A[i*n+j]*x[j];
-        }
-    }
-
-    // norma do vetor
-    for (i=0; i<n; ++i)
-        sum += r[i] * r[i];
-    norm = sqrt(sum);
-
-    return norm;
-}
 
 int main(int argc, char **argv) {
     /** Leitura dos parâmetros de entrada FIXME
