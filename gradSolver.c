64e2c6c8889a8b5b8ea51296164dc8f4c2f0b18c
Kevin Katzer <kk11@inf.ufpr.br>
2014-10-10 16:39:47 -0300

    Adiciona arquivos mat_utils.c e mat_utils.h
    Biblioteca de funções do professor
    
    Signed-off-by: Kevin Katzer <kk11@inf.ufpr.br>

--- gradSolver.c
+++ gradSolver.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <sys/time.h>
 #include <string.h>
+#include "mat_utils.h"
+
 
 /**
  * @file gradSolver.c
@@ -36,63 +38,6 @@ double calcResidualNorm(double *A, double *b, double *x, int n) {
 
     return norm;
 }
-
-/**
- * @brief Função para medição de tempo.
- * @param void ??
- */
-double timestamp(void){
-    struct timeval tp;
-    gettimeofday(&tp, NULL);
-    return((double)(tp.tv_sec + tp.tv_usec/1000000.0));
-}
-
-/**
- * @brief Gera matrizes N*N da forma Ax = b positivas definidas.
- * @param N Dimensão da matriz.
- * @param *A Matriz A.
- * @param *b Vetor b.
- */
-int generateRandomPositiveDefiniteLinearSystem(unsigned int N, double *A, double *b)
-{
-    if ( !A || !b )
-        return (-1);
-
-    /* generate a randomly initialized matrix in row-major order */
-    double *ptr = A;
-    double *end = A + N*N;
-
-    double invRandMax = 1.0/(double)RAND_MAX;
-
-    while( ptr != end ) {
-        *ptr++ = (double)rand() * invRandMax;
-    }
-
-    /* Now we want to make this matrix positive definite. Since all
-       values are positive and <1.0, we have to ensure it is symmetric
-       and diagonally dominant.
-
-       A = A + transpose(A)
-       A = A + I*N                        */
-    unsigned int i,j;
-    for (i=0; i<N; ++i)
-        for (j=i+1; j<N; ++j)
-        {
-            double aux = A[i*N+j];
-            A[i*N+j] += A[j*N+i];
-            A[j*N+i] += aux;
-        }
-
-    for (i=0; i<N; ++i)
-        A[i*N+i] += A[i*N+i] + N;
-
-    /* create the vector of independent terms (b) */
-    for (i=0; i<N; ++i)
-        b[i] = (double)rand() * invRandMax;
-
-    return 0;
-}
-
 
 int main(int argc, char **argv) {
     
