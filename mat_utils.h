98eb95ff75ef0f2d671e69829ce5e765debd0732
Kevin Katzer <kk11@inf.ufpr.br>
2014-10-10 16:39:47 -0300

    Adiciona arquivos mat_utils.c e mat_utils.h
    Biblioteca de funções do professor
    
    Signed-off-by: Kevin Katzer <kk11@inf.ufpr.br>

---
+++ mat_utils.h
@@ -0,0 +1,19 @@
+#ifndef MAT_UTILS
+#define MAT_UTILS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+
+/**
+ * @file mat_utils.h
+ * @brief Header do arquivo mat_utils.c
+ * @author Aryane Ast dos Santos
+ * @author Kevin Katzer
+ */
+
+double timestamp(void);
+int generateRandomPositiveDefiniteLinearSystem(unsigned int N, double *A, double *b);
+
+#endif
