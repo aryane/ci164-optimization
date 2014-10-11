edcda9f730888d40b5be182611618c777145c858
Aryane Ast dos Santos <aryaneast@gmail.com>
2014-10-11 13:11:55 -0300

    grad.c: Quebra o texto do helper em várias linhas
    Signed-off-by: Aryane Ast dos Santos <aryaneast@gmail.com>

--- grad.c
+++ grad.c
@@ -37,5 +37,25 @@ double calcResidualNorm(double *A, double *b, double *x, int n) {
  * @brief Imprime os parâmetros possíveis em stdout
  */
 void helper() {
-    printf ("\033[1mNOME\033[0m\n\tgradSolver - Soluciona um Sistema Linear pelo Método do Gradiente\n\n\033[1mSINOPSE\033[0m\n\t./gradSolver [OPTION]...\n\n\033[1mDESCRIÇÃO\033[0m\n\tResolve um sistema linear a partir de uma matriz quadrada definida, positiva e simétrica.\n\n\tParâmetros possíveis:\n\n\t\033[1m-i\033[0m\n\t\tArquivo de entrada do programa. Se não incluído, a matriz deve ser inserida via stdin.\tgradSolver -i [endereço do arquivo]\n\n\t\033[1m-o\033[0m\n\t\tArquivo de saída do programa. Se não incluído, a matriz será impressa em stdout.\tgradSolver -o [endereço do arquivo]\n\n\t\033[1m-r\033[0m\n\t\tDimensão da matriz a ser gerada aleatoriamente. Não deve ser inserido se for fornecido um arquivo de entrada.\tgradSolver -r [dimensão]\n\n\t\033[1m-k\033[0m\n\t\tNúmero máximo de iterações a serem executadas. (Opcional)\tgradSolver -k [número de iterações]\n\n\t\033[1m-e\033[0m\n\t\tO maior erro aceitável. (Opcional)\tgradSolver -e [erro]\n\n");
+    printf("\033[1mNOME\033[0m\n");
+    printf("\tgradSolver - Soluciona um Sistema Linear pelo Método do Gradiente\n");
+    printf("\033[1mSINOPSE\033[0m\n");
+    printf("\t./gradSolver [OPTION]...\n");
+    printf("\033[1mDESCRIÇÃO\033[0m\n");
+    printf("\tResolve um sistema linear a partir de uma matriz quadrada definida, positiva e simétrica.\n");
+    printf("\tParâmetros possíveis:\n");
+    printf("\t\033[1m-i\033[0m\n");
+    printf("\t\tArquivo de entrada do programa. Se não incluído, a matriz deve ser inserida via stdin.");
+    printf("\tgradSolver -i [endereço do arquivo]\n");
+    printf("\t\033[1m-o\033[0m\n");
+    printf("\t\tArquivo de saída do programa. Se não incluído, a matriz será impressa em stdout.");
+    printf("\tgradSolver -o [endereço do arquivo]\n");
+    printf("\t\033[1m-r\033[0m\n");
+    printf("\t\tDimensão da matriz a ser gerada aleatoriamente. Não deve ser inserido se for fornecido um arquivo de entrada.");
+    printf("\tgradSolver -r [dimensão]\n");
+    printf("\t\033[1m-k\033[0m\n");
+    printf("\t\tNúmero máximo de iterações a serem executadas. (Opcional)");
+    printf("\tgradSolver -k [número de iterações]\n");
+    printf("\t\033[1m-e\033[0m\n");
+    printf("\t\tO maior erro aceitável. (Opcional)\tgradSolver -e [erro]\n");
 }
