e70379426dc38ad28674fb60e0ecfcffa2add86b
Aryane Ast dos Santos <aryaneast@gmail.com>
2014-10-11 18:12:40 -0300

    main.c: Parsing dos argumentos com getopt
    Signed-off-by: Aryane Ast dos Santos <aryaneast@gmail.com>

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <sys/time.h>
-#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+
 #include "mat_utils.h"
 #include "grad.h"
 
@@ -14,14 +16,53 @@
  */
 
 int main(int argc, char **argv) {
-    /** Leitura dos parâmetros de entrada FIXME
-    for (int i=1; i<argc; i += 2){
-        if (argv[i][1] == 'i')
-        else if (argv[i][1] == 'o')
-        else if (argv[i][1] == 'r')
-        else if (argv[i][1] == 'k')
-        else if (argv[i][1] == 'e')
-    }*/
+
+    char *ivalue = NULL;
+    char *ovalue = NULL;
+    char *rvalue = NULL;
+    char *kvalue = NULL;
+    char *evalue = NULL;
+    int index;
+    int c;
+
+    while ((c = getopt(argc, argv, "i:o:r:k:e:")) != -1) {
+        switch (c) {
+            case 'i':
+                ivalue = optarg;
+                break;
+            case 'o':
+                ovalue = optarg;
+                break;
+            case 'r':
+                rvalue = optarg;
+                break;
+            case 'k':
+                kvalue = optarg;
+                break;
+            case 'e':
+                evalue = optarg;
+                break;
+            case '?':
+                if ((optopt == 'k') || (optopt == 'e') || (optopt == 'i') ||
+                        (optopt == 'o') || (optopt == 'r'))
+                    fprintf (stderr, "Opção -%c requere um argumento.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr, "Opção desconhecida `-%c'.\n", optopt);
+                else
+                    fprintf (stderr,
+                            "Character de opção desconhecido `\\x%x'.\n",
+                            optopt);
+                    helper();
+                exit(1);
+            default:
+                abort();
+        }
+    }
+    for (index = optind; index < argc; index++)
+        printf("Argumento %s não é opção.\n", argv[index]);
+
+    /* printf ("ivalue = %s, ovalue = %s, rvalue = %s, kvalue = %s, evalue = %s\n",
+      ivalue, ovalue, rvalue, kvalue, evalue); */
 
     return 0;
 }
